8f2ce4d88d01b62d9cea23638fd5fd767825dc3b
buchara003 <spbvladikspb@gmail.com>
2026-07-05 01:06:38 +0300

    Update update.py

--- update.py
+++ update.py
@@ -5,12 +5,7 @@ import requests
 API_KEY = os.environ["HAPPY_API_KEY"]
 
 TELEGRAM_URL = "https://t.me/s/happvpn"
-
 API_URL = "https://happy-decoder.cc/api/v1/decrypt"
-
-HEADERS = {
-    "Authorization": f"Bearer {API_KEY}"
-}
 
 
 def get_last_links():
@@ -25,16 +20,23 @@ def get_last_links():
     if not links:
         raise Exception("Не найдено ни одного happ://crypt5")
 
+    print(f"Найдено {len(links)} ссылок")
+
     return links[-2:]
 
 
 def decrypt(link):
-    print("Расшифровываю...")
+    print("Расшифровываю:")
+    print(link[:80] + "...")
 
     r = requests.post(
         API_URL,
-        headers=HEADERS,
-        json={"url": link},
+        headers={
+            "Authorization": f"Bearer {API_KEY}"
+        },
+        json={
+            "url": link
+        },
         timeout=20,
     )
 
@@ -48,68 +50,42 @@ def decrypt(link):
     return data["decryptedUrl"]
 
 
-def download_subscription(url):
+def download(url):
     print("Скачиваю подписку...")
+    print(url)
 
     r = requests.get(url, timeout=30)
 
     r.raise_for_status()
 
+    print("Размер:", len(r.text), "символов")
+
     return r.text
-
-
-def extract_configs(text):
-    configs = []
-
-    for line in text.splitlines():
-        line = line.strip()
-
-        if line.startswith((
-            "vmess://",
-            "vless://",
-            "trojan://",
-            "ss://",
-            "ssr://",
-            "tuic://",
-            "hy2://",
-            "hysteria://",
-            "hysteria2://"
-        )):
-            configs.append(line)
-
-    return configs
 
 
 def main():
 
-    all_configs = []
+    result = []
 
     links = get_last_links()
-
-    print(f"Найдено {len(links)} crypt5")
 
     for link in links:
 
         try:
-            sub_url = decrypt(link)
 
-            print(sub_url)
+            sub = decrypt(link)
 
-            text = download_subscription(sub_url)
+            text = download(sub)
 
-            configs = extract_configs(text)
-
-            all_configs.extend(configs)
+            result.append(text)
 
         except Exception as e:
-            print(e)
-
-    all_configs = list(dict.fromkeys(all_configs))
+            print("Ошибка:", e)
 
     with open("sobr.txt", "w", encoding="utf-8") as f:
-        f.write("\n".join(all_configs))
+        f.write("\n\n".join(result))
 
-    print(f"Сохранено {len(all_configs)} конфигов")
+    print("Готово.")
 
 
 if __name__ == "__main__":
